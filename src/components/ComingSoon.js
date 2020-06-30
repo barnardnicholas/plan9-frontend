d41d43fd5ffc9ec901a8819f2282a87888f6e202
barnardnicholas <barnard.nicholas@googlemail.com>
2020-06-30 21:48:54 +0100

    remove extra assets

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import plan9poster from "../assets/plan9poster.jpg";
 
 export default function ComingSoon() {
   return (
@@ -7,6 +8,13 @@ export default function ComingSoon() {
       <Row>
         <Col>
           <center>
+            <img
+              src={plan9poster}
+              alt="Plan 9 From Outer Space"
+              height="50%"
+              width="auto"
+              style={{ margin: "30px" }}
+            />
             <h1>Coming Soon...</h1>
           </center>
         </Col>
